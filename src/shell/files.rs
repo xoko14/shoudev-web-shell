4afe1b0fdf2d0f41dcd972bc5128ac416a204978
xoko14 <xoquinperezb@gmail.com>
2023-04-29 17:54:33 +0200

    ls and cat

--- src/shell/files.rs
+++ src/shell/files.rs
@@ -1,4 +1,70 @@
-pub struct File {
+use std::fmt::Write;
+
+use crate::statics::FILES;
+
+use super::ShellResult;
+
+pub struct SimFile {
     pub name: String,
     pub contents: String,
+    pub permissions: FilePerms,
+}
+
+pub struct FilePerms {
+    pub r: bool,
+    pub w: bool,
+    pub x: bool,
+}
+
+impl Default for SimFile {
+    fn default() -> Self {
+        Self {
+            name: String::default(),
+            contents: String::default(),
+            permissions: FilePerms::default(),
+        }
+    }
+}
+
+impl Default for FilePerms {
+    fn default() -> Self {
+        Self {
+            r: true,
+            w: true,
+            x: false,
+        }
+    }
+}
+
+pub fn ls(args: Vec<&str>) -> ShellResult {
+    let mut output: String = String::new();
+
+    if args.contains(&"-l") {
+        for file in FILES.iter() {
+            let read = if file.permissions.r { "r" } else { "-" };
+            let write = if file.permissions.w { "w" } else { "-" };
+            let execute = if file.permissions.x { "x" } else { "-" };
+            write!(output, "{}{}{} {}\n", read, write, execute, file.name)?;
+        }
+    } else {
+        for file in FILES.iter() {
+            write!(output, "{} ", file.name)?;
+        }
+    }
+    Ok(output)
+}
+
+pub fn cat(args: Vec<&str>) -> ShellResult {
+    if args.len() != 1 {
+        return Ok("This command only accepts one argument.".to_owned());
+    }
+
+    let filename = args[0];
+
+    let file = FILES.iter().find(|f| f.name == filename);
+
+    match file {
+        Some(f) => Ok(f.contents.to_owned()),
+        None => Ok(format!("File \"{}\" not found.", filename)),
+    }
 }
--- src/statics.rs
+++ src/statics.rs
@@ -1,3 +1,7 @@
+use lazy_static::lazy_static;
+
+use crate::shell::files::SimFile;
+
 pub static SHELL_STATUS: &'static str =
     "╭─<span class=green>you@shoudev.com</span> in <span class=blue>~</span>";
 pub static SHELL_PROMPT: &'static str = "╰─$ ";
@@ -12,3 +16,17 @@ pub static LOGO: &'static str = r#"       __                          __
 
 Type <span class=orange>help</span> to see all avaliable commands."#;
 
+lazy_static! {
+    pub static ref FILES: Vec<SimFile> = vec![
+        SimFile {
+            name: "test".to_owned(),
+            contents: "Hi this is a test!\n".to_owned(),
+            ..Default::default()
+        },
+        SimFile {
+            name: "test2".to_owned(),
+            contents: "Hi this is a second test\n".to_owned(),
+            ..Default::default()
+        }
+    ];
+}
